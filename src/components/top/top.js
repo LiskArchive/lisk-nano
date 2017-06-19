146b23c0d4c299d20f98b39b0ece7657176cd996
Aleksey Popov <alepooop@gmail.com>
2017-06-19 22:30:26 +0300

    Add currency dropdown

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -9,14 +9,22 @@ import './top.less';
 app.component('top', {
   template: require('./top.pug')(),
   controller: class top {
-    constructor($scope, Peers, Account) {
+    constructor($scope, $rootScope, Peers, Account) {
       this.peers = Peers;
       this.account = Account;
+      this.$rootScope = $rootScope;
 
       $scope.$on('accountChange', () => {
         this.totalSendable = this.account.get().balance > 1e7 ?
           this.account.get().balance - 1e7 : 0;
       });
+    }
+
+    setCurrency(symbol) {
+      this.$rootScope.symbol = symbol;
+      if (localStorage) {
+        localStorage.setItem('lisk-currency', symbol);
+      }
     }
   },
 });
