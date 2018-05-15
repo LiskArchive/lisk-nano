8bc445b431fccc8298653d80b20e942a5614e4e4
Vit Stanislav <vit@lightcurve.io>
2018-05-15 17:02:57 +0200

    Don't show error toast when invalid link opened
    because it doesn't tell the user what exactly is wrong with the url
    and because it causes issues in auto updater #1062

--- src/utils/externalLinks.js
+++ src/utils/externalLinks.js
@@ -1,8 +1,5 @@
-import i18next from 'i18next';
 import history from '../history';
 import routesReg from './routes';
-import { errorToastDisplayed } from '../actions/toaster';
-import store from '../store';
 
 export default {
   init: () => {
@@ -14,8 +11,6 @@ export default {
         const route = routesReg.find(item => item.regex.test(normalizedUrl));
         if (route !== undefined) {
           history.push(normalizedUrl);
-        } else {
-          store.dispatch(errorToastDisplayed({ label: i18next.t('The URL was invalid') }));
         }
       });
     }
