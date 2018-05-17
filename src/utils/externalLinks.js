769984f813ada17ca07bb7c730fbf88286a6eb27
Vit Stanislav <vit@lightcurve.io>
2018-05-17 11:20:14 +0200

    Allow uppercase 'LISK' in launch protocol
    ... and display error toast only if lisk:// launch protocol and
    invalid URL was detected

--- src/utils/externalLinks.js
+++ src/utils/externalLinks.js
@@ -1,5 +1,8 @@
+import i18next from 'i18next';
 import history from '../history';
 import routesReg from './routes';
+import { errorToastDisplayed } from '../actions/toaster';
+import store from '../store';
 
 export default {
   init: () => {
@@ -7,10 +10,15 @@ export default {
 
     if (ipc) {
       ipc.on('openUrl', (action, url) => {
-        const normalizedUrl = url.replace('lisk://', '/');
-        const route = routesReg.find(item => item.regex.test(normalizedUrl));
-        if (route !== undefined) {
-          history.push(normalizedUrl);
+        const protocol = url.split(':/')[0];
+        const normalizedUrl = url.split(':/')[1];
+        if (protocol && protocol.toLowerCase() === 'lisk' && normalizedUrl) {
+          const route = routesReg.find(item => item.regex.test(normalizedUrl));
+          if (route !== undefined) {
+            history.push(normalizedUrl);
+          } else {
+            store.dispatch(errorToastDisplayed({ label: i18next.t('The URL was invalid') }));
+          }
         }
       });
     }
