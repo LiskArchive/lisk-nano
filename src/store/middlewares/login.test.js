fb9b9cc56d651df97cc33d82e7569e5a4a4faa82
Vit Stanislav <vit@lightcurve.io>
2018-05-23 13:44:22 +0200

    Fix an eslint violation after rebase

--- src/store/middlewares/login.test.js
+++ src/store/middlewares/login.test.js
@@ -10,7 +10,6 @@ describe('Login middleware', () => {
   let store;
   let next;
   const passphrase = 'wagon stock borrow episode laundry kitten salute link globe zero feed marble';
-  const nethash = '198f2b61a8eb95fbeed58b8216780b68f697f26b849acf00c8c93bb9b24f783d';
   const activePeer = new Lisk.APIClient(['http://localhost:4000'], {});
 
   const activePeerSetAction = {
